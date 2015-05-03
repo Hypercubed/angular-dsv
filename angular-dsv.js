832a4046d78f6e8986cadf62919c5c974524599c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2015-05-03 00:05:20 -0700

    change method back

--- angular-dsv.js
+++ angular-dsv.js
@@ -140,7 +140,7 @@
         var params = parseParams(requestConfig, f);
 
         var config = {
-          method: 'post',
+          method: 'get',
           url: url,
           transformResponse: function(data) {
             return dsv.parseRows(data, params.f);
