a919dc4e2ad4eca5d2c5e09c4841607e864152f2
J. Harshbarger <hypercubed@gmail.com>
2015-11-24 14:41:04 +0900

    Use d3-dsv

--- angular-dsv.js
+++ angular-dsv.js
@@ -3,98 +3,7 @@
 
 'use strict';
 
-function d3_dsv_shim (delimiter) {
-  var delimiterCode = delimiter.charCodeAt(0);
-
-  var dsv = {};
-
-  // from https://github.com/mbostock/d3/blob/master/src/dsv/dsv.js
-  dsv.parseRows = function (text, f) {
-    /* jshint -W004: false, -W030: false */
-    var EOL = {}; // sentinel value for end-of-line
-    var EOF = {}; // sentinel value for end-of-file
-    var rows = []; // output rows
-    var N = text.length;
-    var I = 0; // current character index
-    var n = 0; // the current line number
-    var t; // the current token
-    var eol; // is the current token followed by EOL?
-
-    function token () {
-      if (I >= N) return EOF; // special case: end of file
-      if (eol) {
-        eol = false;
-        return EOL;
-      } // special case: end of line
-
-      // special case: quotes
-      var j = I;
-      if (text.charCodeAt(j) === 34) {
-        var i = j;
-        while (i++ < N) {
-          if (text.charCodeAt(i) === 34) {
-            if (text.charCodeAt(i + 1) !== 34) break;
-            ++i;
-          }
-        }
-        I = i + 2;
-        var c = text.charCodeAt(i + 1);
-        if (c === 13) {
-          eol = true;
-          if (text.charCodeAt(i + 2) === 10) ++I;
-        } else if (c === 10) {
-          eol = true;
-        }
-        return text.substring(j + 1, i).replace(/""/g, '\"');
-      }
-
-      // common case: find next delimiter or newline
-      while (I < N) {
-        c = text.charCodeAt(I++);
-        var k = 1;
-        if (c === 10) eol = true; // \n
-        else if (c === 13) {
-          eol = true;
-          if (text.charCodeAt(I) === 10) {
-            ++I;
-            ++k;
-          }
-        } // \r|\r\n
-        else if (c !== delimiterCode) continue;
-        return text.substring(j, I - k);
-      }
-
-      // special case: last token before EOF
-      return text.substring(j);
-    }
-
-    while ((t = token()) !== EOF) {
-      var a = [];
-      while (t !== EOL && t !== EOF) {
-        a.push(t);
-        t = token();
-      }
-      if (f && !(a = f(a, n++))) continue;
-      rows.push(a);
-    }
-
-    return rows;
-  };
-
-  dsv.parse = function (data, f) {
-    /*eslint no-new-func: 0*/
-    var o;
-    return dsv.parseRows(data, function (row, i) {
-      if (o) return o(row, i - 1);
-      var a = new Function('d', 'return {' + row.map(function (name, i) {
-        return JSON.stringify(name) + ': d[' + i + ']';
-      }).join(',') + '}');
-      o = f ? function (row, i) { return f(a(row), i); } : a;
-    });
-  };
-
-  return dsv;
-}
+var d3_dsv = require('d3-dsv').dsv;
 
 function parseParams (requestConfig, f) {
   return angular.isFunction(requestConfig) ? {
@@ -108,21 +17,21 @@ function parseParams (requestConfig, f) {
 
 function dsvFactory ($http, $window) {
   function factory (delimiter) {
-    var d3_dsv = ($window.d3) ? $window.d3.dsv(delimiter) : d3_dsv_shim(delimiter);
+    var _dsv = d3_dsv(delimiter);
 
     function dsv (requestConfig, f) {
       var config = {
         method: 'get',
         transformResponse: function (data) {
-          return d3_dsv.parse(data, f);
+          return _dsv.parse(data, f);
         }
       };
       angular.extend(config, requestConfig);
       return $http(config);
     }
 
-    dsv.parseRows = d3_dsv.parseRows;
-    dsv.parse = d3_dsv.parse;
+    dsv.parseRows = _dsv.parseRows;
+    dsv.parse = _dsv.parse;
 
     dsv.get = function (url, requestConfig, f) {
       var params = parseParams(requestConfig, f);
@@ -145,7 +54,7 @@ function dsvFactory ($http, $window) {
         method: 'get',
         url: url,
         transformResponse: function (data) {
-          return d3_dsv.parseRows(data, params.f);
+          return _dsv.parseRows(data, params.f);
         }
       };
 
@@ -171,5 +80,138 @@ module.exports = 'hc.dsv';
 angular.module('hc.dsv', [])
   .factory('dsv', dsvFactory);
 
+},{"d3-dsv":2}],2:[function(require,module,exports){
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define('d3-dsv', ['exports'], factory) :
+  factory((global.d3_dsv = {}));
+}(this, function (exports) { 'use strict';
+
+  function dsv(delimiter) {
+    return new Dsv(delimiter);
+  }
+
+  function Dsv(delimiter) {
+    var reFormat = new RegExp("[\"" + delimiter + "\n]"),
+        delimiterCode = delimiter.charCodeAt(0);
+
+    this.parse = function(text, f) {
+      var o;
+      return this.parseRows(text, function(row, i) {
+        if (o) return o(row, i - 1);
+        var a = new Function("d", "return {" + row.map(function(name, i) {
+          return JSON.stringify(name) + ": d[" + i + "]";
+        }).join(",") + "}");
+        o = f ? function(row, i) { return f(a(row), i); } : a;
+      });
+    };
+
+    this.parseRows = function(text, f) {
+      var EOL = {}, // sentinel value for end-of-line
+          EOF = {}, // sentinel value for end-of-file
+          rows = [], // output rows
+          N = text.length,
+          I = 0, // current character index
+          n = 0, // the current line number
+          t, // the current token
+          eol; // is the current token followed by EOL?
+
+      function token() {
+        if (I >= N) return EOF; // special case: end of file
+        if (eol) return eol = false, EOL; // special case: end of line
+
+        // special case: quotes
+        var j = I;
+        if (text.charCodeAt(j) === 34) {
+          var i = j;
+          while (i++ < N) {
+            if (text.charCodeAt(i) === 34) {
+              if (text.charCodeAt(i + 1) !== 34) break;
+              ++i;
+            }
+          }
+          I = i + 2;
+          var c = text.charCodeAt(i + 1);
+          if (c === 13) {
+            eol = true;
+            if (text.charCodeAt(i + 2) === 10) ++I;
+          } else if (c === 10) {
+            eol = true;
+          }
+          return text.slice(j + 1, i).replace(/""/g, "\"");
+        }
+
+        // common case: find next delimiter or newline
+        while (I < N) {
+          var c = text.charCodeAt(I++), k = 1;
+          if (c === 10) eol = true; // \n
+          else if (c === 13) { eol = true; if (text.charCodeAt(I) === 10) ++I, ++k; } // \r|\r\n
+          else if (c !== delimiterCode) continue;
+          return text.slice(j, I - k);
+        }
+
+        // special case: last token before EOF
+        return text.slice(j);
+      }
+
+      while ((t = token()) !== EOF) {
+        var a = [];
+        while (t !== EOL && t !== EOF) {
+          a.push(t);
+          t = token();
+        }
+        if (f && (a = f(a, n++)) == null) continue;
+        rows.push(a);
+      }
+
+      return rows;
+    }
+
+    this.format = function(rows) {
+      if (Array.isArray(rows[0])) return this.formatRows(rows); // deprecated; use formatRows
+      var fieldSet = Object.create(null), fields = [];
+
+      // Compute unique fields in order of discovery.
+      rows.forEach(function(row) {
+        for (var field in row) {
+          if (!((field += "") in fieldSet)) {
+            fields.push(fieldSet[field] = field);
+          }
+        }
+      });
+
+      return [fields.map(formatValue).join(delimiter)].concat(rows.map(function(row) {
+        return fields.map(function(field) {
+          return formatValue(row[field]);
+        }).join(delimiter);
+      })).join("\n");
+    };
+
+    this.formatRows = function(rows) {
+      return rows.map(formatRow).join("\n");
+    };
+
+    function formatRow(row) {
+      return row.map(formatValue).join(delimiter);
+    }
+
+    function formatValue(text) {
+      return reFormat.test(text) ? "\"" + text.replace(/\"/g, "\"\"") + "\"" : text;
+    }
+  };
+
+  dsv.prototype = Dsv.prototype;
+
+  var csv = dsv(",");
+  var tsv = dsv("\t");
+
+  var version = "0.1.9";
+
+  exports.version = version;
+  exports.dsv = dsv;
+  exports.csv = csv;
+  exports.tsv = tsv;
+
+}));
 },{}]},{},[1])(1)
 });
