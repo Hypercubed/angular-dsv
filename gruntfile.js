4e3e8413644dd75bd208c512319ea2ae2bb0af95
J. Harshbarger <hypercubed@gmail.com>
2015-11-24 13:34:00 +0900

    Removed grunt-karma

--- gruntfile.js
+++ gruntfile.js
@@ -33,55 +33,12 @@ module.exports = function(grunt){
       }
     },
 
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js'
-      },
-      once: {
-        configFile: 'karma.conf.js',
-        singleRun: true,
-        browsers: ['PhantomJS'],
-        options: {
-          files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            '<%= pkg.name %>.js',
-            'test/spec/**/*.js'
-          ]
-        }
-      },
-      minified: {
-        configFile: 'karma.conf.js',
-        singleRun: true,
-        browsers: ['PhantomJS'],
-        options: {
-          files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            '<%= pkg.name %>.min.js',
-            'test/spec/**/*.js'
-          ]
-        }
-      },
-      server: {
-        configFile: 'karma.conf.js',
-        singleRun: false,
-        autoWatch: true,
-        browsers: ['PhantomJS']
-      }
-    },
-
-    //'gh-pages': {
-    //  src: ['<%= pkg.name %>.js','<%= pkg.name %>.min.js','bower_components/**/*','example/*']
-   // }
-
   });
 
   require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['test']);
-  grunt.registerTask('test', ['jshint','karma:once','uglify','karma:minified']);
-  grunt.registerTask('publish', ['test','bump-only','uglify','bump-commit']);
+  grunt.registerTask('build', ['uglify']);
+  grunt.registerTask('publish', ['bump-only','uglify','bump-commit']);
 
 };
