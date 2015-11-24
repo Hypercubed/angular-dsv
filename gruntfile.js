37d18d10d91aa2689551865fb4fb814c1685f0e9
J. Harshbarger <hypercubed@gmail.com>
2015-11-24 13:41:24 +0900

    Removed grunt uglify

--- gruntfile.js
+++ gruntfile.js
@@ -7,38 +7,10 @@ module.exports = function(grunt){
     jshint: {
       options: { jshintrc: true },
       all: ['gruntfile.js', '<%= pkg.name %>.js']
-    },
-
-    bump: {
-      options: {
-        files: ['bower.json','package.json'],
-        commit: true,
-        commitMessage: 'release %VERSION%',
-        commitFiles: ['package.json','bower.json','<%= pkg.name %>.min.js'],
-        pushTo: 'origin',
-        push: true,
-      }
-    },
-
-    uglify: {
-      options: {
-        banner: '/*\n * <%= pkg.title || pkg.name %> <%= pkg.version %>\n' +
-          ' * (c) <%= grunt.template.today("yyyy") %> <%= pkg.authors.join(" ") %>\n' +
-          ' * Licensed <%= pkg.license %>\n */\n'
-      },
-      src: {
-        files: {
-          '<%= pkg.name %>.min.js': '<%= pkg.name %>.js'
-        }
-      }
-    },
+    }
 
   });
 
   require('load-grunt-tasks')(grunt);
-
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['uglify']);
-  grunt.registerTask('publish', ['bump-only','uglify','bump-commit']);
 
 };
