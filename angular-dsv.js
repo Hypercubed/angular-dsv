62719fe0f1567ff670c091843eea6da304db8c77
J. Harshbarger <hypercubed@gmail.com>
2015-11-24 14:21:39 +0900

    Using semistandard

--- angular-dsv.js
+++ angular-dsv.js
@@ -3,26 +3,29 @@
 
 'use strict';
 
-function d3_dsv_shim(delimiter) {
+function d3_dsv_shim (delimiter) {
   var delimiterCode = delimiter.charCodeAt(0);
 
   var dsv = {};
 
   // from https://github.com/mbostock/d3/blob/master/src/dsv/dsv.js
-  dsv.parseRows = function(text, f) {
+  dsv.parseRows = function (text, f) {
     /* jshint -W004: false, -W030: false */
-    var EOL = {}, // sentinel value for end-of-line
-        EOF = {}, // sentinel value for end-of-file
-        rows = [], // output rows
-        N = text.length,
-        I = 0, // current character index
-        n = 0, // the current line number
-        t, // the current token
-        eol; // is the current token followed by EOL?
+    var EOL = {}; // sentinel value for end-of-line
+    var EOF = {}; // sentinel value for end-of-file
+    var rows = []; // output rows
+    var N = text.length;
+    var I = 0; // current character index
+    var n = 0; // the current line number
+    var t; // the current token
+    var eol; // is the current token followed by EOL?
 
-    function token() {
+    function token () {
       if (I >= N) return EOF; // special case: end of file
-      if (eol) return eol = false, EOL; // special case: end of line
+      if (eol) {
+        eol = false;
+        return EOL;
+      } // special case: end of line
 
       // special case: quotes
       var j = I;
@@ -42,14 +45,21 @@ function d3_dsv_shim(delimiter) {
         } else if (c === 10) {
           eol = true;
         }
-        return text.substring(j + 1, i).replace(/""/g, "\"");
+        return text.substring(j + 1, i).replace(/""/g, '\"');
       }
 
       // common case: find next delimiter or newline
       while (I < N) {
-        var c = text.charCodeAt(I++), k = 1;
+        c = text.charCodeAt(I++);
+        var k = 1;
         if (c === 10) eol = true; // \n
-        else if (c === 13) { eol = true; if (text.charCodeAt(I) === 10) ++I, ++k; } // \r|\r\n
+        else if (c === 13) {
+          eol = true;
+          if (text.charCodeAt(I) === 10) {
+            ++I;
+            ++k;
+          }
+        } // \r|\r\n
         else if (c !== delimiterCode) continue;
         return text.substring(j, I - k);
       }
@@ -71,41 +81,39 @@ function d3_dsv_shim(delimiter) {
     return rows;
   };
 
-  dsv.parse = function(data, f) {
-    /* jshint -W054: false */
+  dsv.parse = function (data, f) {
+    /*eslint no-new-func: 0*/
     var o;
-    return dsv.parseRows(data, function(row, i) {
+    return dsv.parseRows(data, function (row, i) {
       if (o) return o(row, i - 1);
-      var a = new Function("d", "return {" + row.map(function(name, i) {
-        return JSON.stringify(name) + ": d[" + i + "]";
-      }).join(",") + "}");
-      o = f ? function(row, i) { return f(a(row), i); } : a;
+      var a = new Function('d', 'return {' + row.map(function (name, i) {
+        return JSON.stringify(name) + ': d[' + i + ']';
+      }).join(',') + '}');
+      o = f ? function (row, i) { return f(a(row), i); } : a;
     });
   };
 
   return dsv;
 }
 
-function parseParams(requestConfig, f){
-	return angular.isFunction(requestConfig) ? {
-		requestConfig: null,
-		f: requestConfig
-	} : {
-		requestConfig: requestConfig,
-		f: f
-	};
+function parseParams (requestConfig, f) {
+  return angular.isFunction(requestConfig) ? {
+    requestConfig: null,
+    f: requestConfig
+  } : {
+    requestConfig: requestConfig,
+    f: f
+  };
 }
 
-function dsvFactory($http, $window) {
+function dsvFactory ($http, $window) {
+  function factory (delimiter) {
+    var d3_dsv = ($window.d3) ? $window.d3.dsv(delimiter) : d3_dsv_shim(delimiter);
 
-  function factory(delimiter) {
-
-		var d3_dsv = ($window.d3) ? $window.d3.dsv(delimiter) : d3_dsv_shim(delimiter);
-
-    function dsv(requestConfig, f) {
+    function dsv (requestConfig, f) {
       var config = {
         method: 'get',
-        transformResponse: function(data) {
+        transformResponse: function (data) {
           return d3_dsv.parse(data, f);
         }
       };
@@ -116,7 +124,7 @@ function dsvFactory($http, $window) {
     dsv.parseRows = d3_dsv.parseRows;
     dsv.parse = d3_dsv.parse;
 
-    dsv.get = function(url, requestConfig, f) {
+    dsv.get = function (url, requestConfig, f) {
       var params = parseParams(requestConfig, f);
 
       var config = {
@@ -130,13 +138,13 @@ function dsvFactory($http, $window) {
       );
     };
 
-    dsv.getRows = function(url, requestConfig, f) {
+    dsv.getRows = function (url, requestConfig, f) {
       var params = parseParams(requestConfig, f);
 
       var config = {
         method: 'get',
         url: url,
-        transformResponse: function(data) {
+        transformResponse: function (data) {
           return d3_dsv.parseRows(data, params.f);
         }
       };
@@ -154,7 +162,6 @@ function dsvFactory($http, $window) {
   factory.csv = factory(',');
 
   return factory;
-
 }
 
 dsvFactory.$inject = ['$http', '$window'];
--- test/spec/angular-dsv.js
+++ test/spec/angular-dsv.js
@@ -1,3 +1,4 @@
+/* global describe, it, beforeEach, inject, expect */
 
 // TODO: test ng-include
 
@@ -9,10 +10,9 @@ describe('Factory: dsv', function () {
 
   var dsv, $httpBackend;
 
-  beforeEach(inject(function(_$httpBackend_, _dsv_) {
+  beforeEach(inject(function (_$httpBackend_, _dsv_) {
     dsv = _dsv_;
     $httpBackend = _$httpBackend_;
-
   }));
 
   /* var element,
@@ -32,109 +32,99 @@ describe('Factory: dsv', function () {
 
   })); */
 
-  describe('csv', function() {
-
-    describe('#get', function() {
-      it('should load a file', function(done) {
-
+  describe('csv', function () {
+    describe('#get', function () {
+      it('should load a file', function (done) {
         $httpBackend.expectGET('test.csv')
-          .respond("a,b,c\n1,2,3\n4,5,6\n7,8,9");
+          .respond('a,b,c\n1,2,3\n4,5,6\n7,8,9');
 
-        dsv.csv.get('test.csv').then(function(response) {
+        dsv.csv.get('test.csv').then(function (response) {
           expect(response.data).toEqual([
-            {a: "1", b: "2", c: "3"},
-            {a: "4", b: "5", c: "6"},
-            {a: "7", b: "8", c: "9"}
+            {a: '1', b: '2', c: '3'},
+            {a: '4', b: '5', c: '6'},
+            {a: '7', b: '8', c: '9'}
           ]);
         });
 
         $httpBackend.flush();
-
-      })
+      });
     });
 
-    describe('#getRows', function() {
-      it('should load a file', function(done) {
-
+    describe('#getRows', function () {
+      it('should load a file', function (done) {
         $httpBackend.expectGET('test.csv')
-          .respond("a,b,c\n1,2,3\n4,5,6\n7,8,9");
+          .respond('a,b,c\n1,2,3\n4,5,6\n7,8,9');
 
-        dsv.csv.getRows('test.csv').then(function(response) {
+        dsv.csv.getRows('test.csv').then(function (response) {
           expect(response.data).toEqual([
-            ["a","b","c"],
-            ["1", "2","3"],
-            ["4", "5", "6"],
-            ["7", "8", "9"]
+            ['a', 'b', 'c'],
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9']
           ]);
         });
 
         $httpBackend.flush();
-
-      })
+      });
     });
 
-    describe('#parse', function() {
-      it('returns an array of objects', function() {
-        expect(dsv.csv.parse("a,b,c\n1,2,3\n")).toEqual([{a: "1", b: "2", c: "3"}]);
-      })
+    describe('#parse', function () {
+      it('returns an array of objects', function () {
+        expect(dsv.csv.parse('a,b,c\n1,2,3\n')).toEqual([{a: '1', b: '2', c: '3'}]);
+      });
 
-      it('parses quoted values', function() {
-        expect(dsv.csv.parse("a,b,c\n1,\"he,l\tlo\",3\n")).toEqual([{a: "1", b: "he,l\tlo", c: "3"}]);
-      })
+      it('parses quoted values', function () {
+        expect(dsv.csv.parse('a,b,c\n1,"he,l\tlo",3\n')).toEqual([{a: '1', b: 'he,l\tlo', c: '3'}]);
+      });
 
-      it('parses unix newlines', function() {
-        expect(dsv.csv.parse("a,b,c\n1,2,3\n4,5,\"6\"\n7,8,9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses unix newlines', function () {
+        expect(dsv.csv.parse('a,b,c\n1,2,3\n4,5,"6"\n7,8,9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
 
-      it('parses mac newlines', function() {
-        expect(dsv.csv.parse("a,b,c\r1,2,3\r4,5,\"6\"\r7,8,9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses mac newlines', function () {
+        expect(dsv.csv.parse('a,b,c\r1,2,3\r4,5,"6"\r7,8,9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
 
-      it('parses dos newlines', function() {
-        expect(dsv.csv.parse("a,b,c\r\n1,2,3\r\n4,5,\"6\"\r\n7,8,9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses dos newlines', function () {
+        expect(dsv.csv.parse('a,b,c\r\n1,2,3\r\n4,5,"6"\r\n7,8,9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
+    });
+  });
 
-    })
-  })
-
-  describe('tsv', function() {
-
-    describe('#get', function() {
-      it('should load a file', function(done) {
-
+  describe('tsv', function () {
+    describe('#get', function () {
+      it('should load a file', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.get('test.tsv').success(function(data) {
+        dsv.tsv.get('test.tsv').success(function (data) {
           expect(data).toEqual([
-            {a: "1", b: "2", c: "3"},
-            {a: "4", b: "5", c: "6"},
-            {a: "7", b: "8", c: "9"}
+            {a: '1', b: '2', c: '3'},
+            {a: '4', b: '5', c: '6'},
+            {a: '7', b: '8', c: '9'}
           ]);
         });
 
         $httpBackend.flush();
+      });
 
-      })
-
-      it('should accept a config object and accessor function', function(done) {
-
+      it('should accept a config object and accessor function', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.get('test.tsv', { cache: true }, function(d) { return (+d.a + +d.b + +d.c) }).success(function(data) {
+        dsv.tsv.get('test.tsv', { cache: true }, function (d) { return (+d.a + +d.b + +d.c); }).success(function (data) {
           expect(data).toEqual([
             6,
             15,
@@ -143,15 +133,13 @@ describe('Factory: dsv', function () {
         });
 
         $httpBackend.flush();
+      });
 
-      })
-
-      it('config object should be optional', function(done) {
-
+      it('config object should be optional', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.get('test.tsv', function(d) { return (+d.a + +d.b + +d.c) }).success(function(data) {
+        dsv.tsv.get('test.tsv', function (d) { return (+d.a + +d.b + +d.c); }).success(function (data) {
           expect(data).toEqual([
             6,
             15,
@@ -160,115 +148,102 @@ describe('Factory: dsv', function () {
         });
 
         $httpBackend.flush();
-
-      })
-
+      });
     });
 
-    describe('#getRows', function() {
-      it('should load a file', function(done) {
-
+    describe('#getRows', function () {
+      it('should load a file', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.getRows('test.tsv').success(function(data) {
+        dsv.tsv.getRows('test.tsv').success(function (data) {
           expect(data).toEqual([
-            ["a","b","c"],
-            ["1", "2","3"],
-            ["4", "5", "6"],
-            ["7", "8", "9"]
+            ['a', 'b', 'c'],
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9']
           ]);
         });
 
         $httpBackend.flush();
+      });
 
-      })
-
-      it('should accept a config object and accessor function', function(done) {
-
+      it('should accept a config object and accessor function', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.getRows('test.tsv',{}, function(d) { return { values: d }; }).success(function(data) {
+        dsv.tsv.getRows('test.tsv', {}, function (d) { return { values: d }; }).success(function (data) {
           expect(data).toEqual([
-            { values: ["a","b","c"] },
-            { values: ["1", "2","3"] },
-            { values: ["4", "5", "6"] },
-            { values: ["7", "8", "9"] }
+            { values: ['a', 'b', 'c'] },
+            { values: ['1', '2', '3'] },
+            { values: ['4', '5', '6'] },
+            { values: ['7', '8', '9'] }
           ]);
         });
 
         $httpBackend.flush();
+      });
 
-      })
-
-      it('config object should be optional', function(done) {
-
+      it('config object should be optional', function (done) {
         $httpBackend.expectGET('test.tsv')
-          .respond("a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+          .respond('a\tb\tc\n1\t2\t3\n4\t5\t6\n7\t8\t9');
 
-        dsv.tsv.getRows('test.tsv', function(d) { return { values: d }; }).success(function(data) {
+        dsv.tsv.getRows('test.tsv', function (d) { return { values: d }; }).success(function (data) {
           expect(data).toEqual([
-            { values: ["a","b","c"] },
-            { values: ["1", "2","3"] },
-            { values: ["4", "5", "6"] },
-            { values: ["7", "8", "9"] }
+            { values: ['a', 'b', 'c'] },
+            { values: ['1', '2', '3'] },
+            { values: ['4', '5', '6'] },
+            { values: ['7', '8', '9'] }
           ]);
         });
 
         $httpBackend.flush();
-
-      })
+      });
     });
 
-    describe('#parse', function() {
-      it('returns an array of objects', function() {
-        expect(dsv.tsv.parse("a\tb\tc\n1\t2\t3\n")).toEqual([{a: "1", b: "2", c: "3"}]);
-      })
+    describe('#parse', function () {
+      it('returns an array of objects', function () {
+        expect(dsv.tsv.parse('a\tb\tc\n1\t2\t3\n')).toEqual([{a: '1', b: '2', c: '3'}]);
+      });
 
-      it('parses quoted values', function() {
-        expect(dsv.tsv.parse("a\tb\tc\n1\t\"he,l\tlo\"\t3\n")).toEqual([{a: "1", b: "he,l\tlo", c: "3"}]);
-      })
+      it('parses quoted values', function () {
+        expect(dsv.tsv.parse('a\tb\tc\n1\t"he,l\tlo"\t3\n')).toEqual([{a: '1', b: 'he,l\tlo', c: '3'}]);
+      });
 
-      it('parses unix newlines', function() {
-        expect(dsv.tsv.parse("a\tb\tc\n1\t2\t3\n4\t5\t\"6\"\n7\t8\t9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses unix newlines', function () {
+        expect(dsv.tsv.parse('a\tb\tc\n1\t2\t3\n4\t5\t"6"\n7\t8\t9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
 
-      it('parses mac newlines', function() {
-        expect(dsv.tsv.parse("a\tb\tc\r1\t2\t3\r4\t5\t\"6\"\r7\t8\t9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses mac newlines', function () {
+        expect(dsv.tsv.parse('a\tb\tc\r1\t2\t3\r4\t5\t"6"\r7\t8\t9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
 
-      it('parses dos newlines', function() {
-        expect(dsv.tsv.parse("a\tb\tc\r\n1\t2\t3\r\n4\t5\t\"6\"\r\n7\t8\t9")).toEqual([
-          {a: "1", b: "2", c: "3"},
-          {a: "4", b: "5", c: "6"},
-          {a: "7", b: "8", c: "9"}
+      it('parses dos newlines', function () {
+        expect(dsv.tsv.parse('a\tb\tc\r\n1\t2\t3\r\n4\t5\t"6"\r\n7\t8\t9')).toEqual([
+          {a: '1', b: '2', c: '3'},
+          {a: '4', b: '5', c: '6'},
+          {a: '7', b: '8', c: '9'}
         ]);
       });
-    })
+    });
 
-    describe('#parseRows', function() {
-
-      it('returns an array of arrays', function() {
-        expect(dsv.tsv.parseRows("a\tb\tc\n1\t2\t3\n4\t5\t\"6\"\n7\t8\t9")).toEqual([
-          ["a","b","c"],
-          ["1", "2","3"],
-          ["4", "5", "6"],
-          ["7", "8", "9"]
+    describe('#parseRows', function () {
+      it('returns an array of arrays', function () {
+        expect(dsv.tsv.parseRows('a\tb\tc\n1\t2\t3\n4\t5\t"6"\n7\t8\t9')).toEqual([
+          ['a', 'b', 'c'],
+          ['1', '2', '3'],
+          ['4', '5', '6'],
+          ['7', '8', '9']
         ]);
       });
-
-    })
-  })
-
-
-
+    });
+  });
 });
